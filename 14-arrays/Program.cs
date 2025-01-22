00f3fb82e659a4fd854dbda7667b17227f5c69f8
supekar-aniket <supekaraniket34@gmail.com>
2025-01-23 00:06:07 +0530

    Add arrays , methods

---
+++ 14-arrays/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Arrays : a variable that can store multiple values of same datatype.
+                    - arrays is a fixed size.
+            */
+
+            string[] cars = new string[3];
+            cars[0] = "BMW";
+            cars[1] = "Mustang";
+            cars[2] = "Corvette";
+
+            // System.Console.WriteLine(cars[0]);
+            // System.Console.WriteLine(cars[1]);
+            // System.Console.WriteLine(cars[2]);
+
+            // for(int i=0; i<cars.Length; i++) {
+            //     System.Console.WriteLine(cars[i]);
+            // }
+
+            foreach (string c in cars)
+            {
+                System.Console.WriteLine(c);
+            }
+        }
+    }
+}
---
+++ 15-methods/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            method :
+                - it is a bunch of code, that run when it called.
+                - it is a reuseable code
+            */
+
+            methodWithoutArgument();
+
+            string name = "Agastya";
+            // method with argument
+            happyBirthday(name);
+        }
+
+        static void methodWithoutArgument(){
+            // Code
+            System.Console.WriteLine("Hello");
+        }
+
+        static void happyBirthday(string birthdayBoy){
+            System.Console.WriteLine("Happy Birthday to you!");
+            System.Console.WriteLine("Happy Birthday to you!");
+            System.Console.WriteLine($"Happy Birthday dear {birthdayBoy}");
+            System.Console.WriteLine("Happy Birthday to you!");
+        }
+    }
+}
