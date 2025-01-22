01318f3870690c46cb258384e87eb6fec00ff825
supekar-aniket <supekaraniket34@gmail.com>
2025-01-23 01:58:32 +0530

    Use of return keyword

---
+++ 16-return_keyword/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            return : return data back to the place where method called.
+
+            */
+
+            System.Console.WriteLine("Enter first number:");
+            double a = Convert.ToDouble(Console.ReadLine());
+            
+            System.Console.WriteLine("Enter second number:");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            double result = multiply(a,b);
+
+            System.Console.WriteLine($"Your Result is:{result}");
+
+        }
+
+        static double multiply(double x,double y){
+            
+            // double z = x * y;
+
+            return x * y;
+        }
+    }
+}
