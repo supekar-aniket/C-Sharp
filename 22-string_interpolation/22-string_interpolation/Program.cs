6ac717f6012fe03d7f555f1eb374a9eabebcdaa6
supekar-aniket <supekaraniket34@gmail.com>
2025-01-29 22:52:06 +0530

    add string interpolation

---
+++ 22-string_interpolation/22-string_interpolation/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             string interpolation :
+                        - allows us to insert variables into a string literal
+                        - string literal with 
+                        - and {} with values
+                        - Ex :
+                            string name = "Alice";
+                            int age = 25;
+                            string message = $"Name: {name}, Age: {age}";
+             */
+
+            string firstName = "Agastya";
+            string lastName = "Supekar";
+            int age = 22;
+
+            Console.WriteLine("Hello " + firstName + " " + lastName + " you are " + age + "years old");
+
+            Console.ReadKey();
+        }
+
+    }
+}
