e9ac7c847d8a9b9604efedda2347c20810945876
supekar-aniket <supekaraniket34@gmail.com>
2025-01-29 22:54:32 +0530

    add how use of objects

---
+++ 25-objects/25-objects/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             objects :
+                    - is an instance of a class
+                    - A class can use as a blueprint to create a objects (OOP)
+                    - objects can have fields and methods
+             */
+
+            Human human1 = new Human();
+            human1.name = "Agastya";
+
+            human1.Eat();
+            human1.Sleep();
+
+            Human human2 = new Human();
+            human2.name = "Arpita";
+
+            human2.Eat();
+            human2.Sleep();
+
+            Console.ReadKey();
+        }
+
+        class Human
+        {
+            public string name;
+
+            public void Eat()
+            {
+                Console.WriteLine($"{name} is eating!!!");
+            }
+
+            public void Sleep()
+            {
+                Console.WriteLine($"{name} is Sleeping!!!");
+            }
+        }
+    }
+}
