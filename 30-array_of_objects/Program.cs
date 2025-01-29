b733e6c69a93fba48adae691a887ef9c1508fbca
supekar-aniket <supekaraniket34@gmail.com>
2025-01-30 00:06:11 +0530

    how to array as a object

---
+++ 30-array_of_objects/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Car[] garrage = new Car[3];
+
+            //Car car1 = new Car("Mustang");
+            //Car car2 = new Car("BMW");
+            //Car car3 = new Car("Totota");
+
+            //garrage[0] = car1;
+            //garrage[1] = car2;
+            //garrage[2] = car3;
+
+            ////Console.WriteLine(garrage[0].model);
+            ////Console.WriteLine(garrage[1].model);
+            ////Console.WriteLine(garrage[2].model);
+
+            //// insted of cw we use foreach loop
+            //foreach(Car car in garrage)
+            //{
+            //    Console.WriteLine(car.model);
+            //}
+
+            // Insted of all above we pass the array as object
+
+            Car[] garrage = { new Car("Mustang"), new Car("BMW"), new Car("Totota")};
+
+            foreach (Car car in garrage)
+            {
+                Console.WriteLine(car.model);
+            }
+
+            Console.ReadKey();
+        }
+    }
+
+    class Car
+    {
+        public string model;
+
+        public Car(string model)
+        {
+            this.model = model;
+        }
+    }
+}
